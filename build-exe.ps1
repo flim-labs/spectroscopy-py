847d102de5a05381eb4764b12d3e4fbf5ca06f7b
Misael Sabbadini <sabbadini.misael@gmail.com>
2024-05-04 16:53:50 +0200

    fix: restore CI-CD (add PyQt6 --force-reinstall)

--- build-exe.ps1
+++ build-exe.ps1
@@ -3,5 +3,6 @@ py -3.9 -m venv .venv-spectroscopy
 python -m pip install --upgrade pip
 pip install pyinstaller
 pip install -r .\requirements.txt
+pip install PyQt6 --force-reinstall
 pyinstaller --noconsole --onefile --icon .\assets\spectroscopy-logo.ico --add-data "assets/*:assets"  .\spectroscopy.py
 deactivate
