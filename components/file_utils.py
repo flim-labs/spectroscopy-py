03179ba431d12857d6c74c65db3de56a19b6ac5a
Aurora Sirigu <aurora.sirigu@5bits.it>
2024-07-29 17:07:54 +0200

    feat: improve download data and scripts

--- components/file_utils.py
+++ components/file_utils.py
@@ -5,7 +5,6 @@ from components.box_message import BoxMessage
 from components.gui_styles import GUIStyles
 
 
-
 def directory_selector(window):
     folder_path = QFileDialog.getExistingDirectory(window, "Select Directory")
     return folder_path
@@ -16,7 +15,9 @@ def get_recent_spectroscopy_file():
     files = [
         f
         for f in os.listdir(data_folder)
-        if f.startswith("spectroscopy") and not ("calibration" in f) and not ("phasors" in f)
+        if f.startswith("spectroscopy")
+        and not ("calibration" in f)
+        and not ("phasors" in f)
     ]
     files.sort(
         key=lambda x: os.path.getmtime(os.path.join(data_folder, x)), reverse=True
@@ -57,28 +58,31 @@ def save_spectroscopy_file(new_filename, dest_path, window):
         window.exported_data_file_paths["spectroscopy"] = destination_file
     except Exception as e:
         print("Error  saving spectroscopy file")
-        
-        
-def save_phasor_files(spectroscopy_new_filename, phasors_new_filename, dest_path, window):
+
+
+def save_phasor_files(
+    spectroscopy_new_filename, phasors_new_filename, dest_path, window
+):
     spectroscopy_ref_source_file = get_recent_spectroscopy_file()
     phasors_source_file = get_recent_phasors_file()
     if not spectroscopy_ref_source_file or not phasors_source_file:
         return
     try:
-        dest_spectroscopy_file_name = rename_bin_file(spectroscopy_ref_source_file, spectroscopy_new_filename)
-        destination_spectroscopy_file = os.path.join(dest_path, dest_spectroscopy_file_name)
+        dest_spectroscopy_file_name = rename_bin_file(
+            spectroscopy_ref_source_file, spectroscopy_new_filename
+        )
+        destination_spectroscopy_file = os.path.join(
+            dest_path, dest_spectroscopy_file_name
+        )
         shutil.copy2(spectroscopy_ref_source_file, destination_spectroscopy_file)
-        dest_phasors_file_name = rename_bin_file(phasors_source_file, phasors_new_filename)
+        dest_phasors_file_name = rename_bin_file(
+            phasors_source_file, phasors_new_filename
+        )
         destination_phasors_file = os.path.join(dest_path, dest_phasors_file_name)
         shutil.copy2(phasors_source_file, destination_phasors_file)
         window.exported_data_file_paths["phasors"] = destination_phasors_file
-        window.exported_data_file_paths["spectroscopy_phasors_ref"] = destination_spectroscopy_file    
+        window.exported_data_file_paths["spectroscopy_phasors_ref"] = (
+            destination_spectroscopy_file
+        )
     except Exception as e:
-        print("Error  saving spectroscopy file")        
-            
-
-
-    
-
-
-
+        print("Error  saving spectroscopy file")
