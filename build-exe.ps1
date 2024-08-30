9ad9dd58e6eee60e44b654b6aba877e93c5ec2d4
Aurora Sirigu <aurora.sirigu@5bits.it>
2024-08-30 16:20:56 +0200

    chore: spectroscopy with updated realtime temp version

--- build-exe.ps1
+++ build-exe.ps1
@@ -3,6 +3,9 @@ py -3.9 -m venv .venv-spectroscopy
 python -m pip install --upgrade pip
 pip install pyinstaller
 pip install -r .\requirements.txt
+pip install flim_labs-1.0.58-cp39-none-win_amd64.whl
 pip install PyQt6 --force-reinstall
+pip install numpy --force-reinstall
+pip install scipy --force-reinstall
 pyinstaller --onefile --icon .\assets\spectroscopy-logo.ico --add-data "assets/*:assets" --add-data "export_data_scripts/*:export_data_scripts" --hidden-import=matplotlib.backends.backend_ps --hidden-import=matplotlib.backends.backend_agg .\spectroscopy.py
 deactivate
