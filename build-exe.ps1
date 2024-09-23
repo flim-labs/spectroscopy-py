70e78fbd2521df2ecdad190416bba4155549beaa
Aurora Sirigu <aurora.sirigu@5bits.it>
2024-09-23 12:59:17 +0200

    chore: update Spectroscopy/Processor versions

--- build-exe.ps1
+++ build-exe.ps1
@@ -3,7 +3,6 @@ py -3.9 -m venv .venv-spectroscopy
 python -m pip install --upgrade pip
 pip install pyinstaller
 pip install -r .\requirements.txt
-pip install flim_labs-1.0.62-cp39-none-win_amd64.whl
 pip install PyQt6 --force-reinstall
 pyinstaller --onefile --icon .\assets\spectroscopy-logo.ico --add-data "assets/*:assets" --add-data "export_data_scripts/*:export_data_scripts" --hidden-import=matplotlib.backends.backend_ps --hidden-import=matplotlib.backends.backend_agg .\spectroscopy.py
 deactivate
