bc39ac7b8a74410468c5c9be9d5d396238675ace
Aurora Sirigu <aurora.sirigu@5bits.it>
2024-11-22 14:37:21 +0100

    fix: fix settings.ini utilities

--- components/settings_utilities.py
+++ components/settings_utilities.py
@@ -3,7 +3,6 @@ from configparser import ConfigParser
 
 from settings import VERSION
 
-
 def check_and_update_ini():
     ini_file = "settings.ini"
     config = ConfigParser()
@@ -13,20 +12,21 @@ def check_and_update_ini():
         if "General" in config:
             stored_version = config["General"].get("app_version", None)
             if stored_version != VERSION:
-                print(
-                    f"Version mismatch: {stored_version} != {VERSION}. Updating '{ini_file}'."
-                )
+                print(f"Version mismatch: {stored_version} != {VERSION}. Updating '{ini_file}'.")
                 recreate_ini_file(ini_file)
         else:
             recreate_ini_file(ini_file)
     else:
         recreate_ini_file(ini_file)
-
+        
 
 def recreate_ini_file(ini_file):
     config = ConfigParser()
     config["General"] = {
         "app_version": VERSION,
     }
-    with open(ini_file, "w") as f:
-        config.write(f)
+    try:
+        with open(ini_file, "w") as f:
+            config.write(f)
+    except PermissionError:
+        pass
