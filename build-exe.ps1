a18cfd5011ac4dbe142d5d6682037627afa0222f
Misael Sabbadini <sabbadini.misael@gmail.com>
2026-02-16 19:20:05 +0100

    fxi: update build-exe.ps1 to use python command for virtual environment and package installations

--- build-exe.ps1
+++ build-exe.ps1
@@ -1,8 +1,8 @@
-py -3.9 -m venv .venv-spectroscopy
+python -m venv .venv-spectroscopy
 .venv-spectroscopy\Scripts\Activate.ps1
 python -m pip install --upgrade pip
-pip install pyinstaller
-pip install -r .\requirements.txt
-pip install PyQt6 --force-reinstall
+python -m pip install pyinstaller
+python -m pip install -r .\requirements.txt
+python -m pip install PyQt6 --force-reinstall
 pyinstaller --onefile --icon .\assets\spectroscopy-logo.ico --add-data "assets/*:assets" --add-data "export_data_scripts/*:export_data_scripts" --hidden-import=matplotlib.backends.backend_ps --hidden-import=matplotlib.backends.backend_agg .\spectroscopy.py
 deactivate
